ff2336292da943471e7721272d6b31a54503facf
1xTMxrioo <168331533+1xTMxrioo@users.noreply.github.com>
2025-01-11 15:32:59 +0530

    Add files via upload

---
+++ DmgBrick.lua
@@ -0,0 +1,22 @@
+local DAMAGE = 3
+local COOLDOWN = 0.09
+
+local parentPart = script.Parent
+
+local recentlyDamagedCharacters = {}
+
+local function onTouch(otherPart)
+	
+	local character = otherPart.Parent
+	local humanoid = character:FindFirstChildWhichIsA("Humanoid")
+	local player = game:GetService("Players"):GetPlayerFromCharacter(character)
+	
+	if player and humanoid and not recentlyDamagedCharacters[character] then
+		humanoid:TakeDamage(DAMAGE)
+		recentlyDamagedCharacters[character] = true
+		wait(COOLDOWN_IN_SECONDS)
+		recentlyDamagedCharacters[character] = nil
+	end
+end
+
+parentPart.Touched:Connect(onTouch)
