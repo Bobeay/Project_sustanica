199b47b6e82f7106b1f13c0bd985119c6c5fdb9e
Bobeay <157194010+Bobeay@users.noreply.github.com>
2025-01-12 05:33:50 +0530

    Add files via upload

---
+++ LocalScriptongod.lua
@@ -0,0 +1,10 @@
+-cript.Parent.Equipped:Connect(function(Mouse)
+    Mouse.Button1Down:Connect(function()
+       animation = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(script.Parent.Animation)
+        animation:Play()
+    end)
+end)
+
+script.Parent.Unequipped:Connect(function()
+        animation:Stop()
+end)
